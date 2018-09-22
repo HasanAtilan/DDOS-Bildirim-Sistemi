edc752bfce8e0a9a55a1f6491b3749f164a6d9e1
HasanAtilan <zb-sunucum@hotmail.com>
2018-09-22 17:24:21 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
 ######Server
-/*	Server IP		*/	$bot['server_ip'] = "127.0.0.1";
-/*	UDP Port		*/	$bot['server_port'] = "9987";
-/*	Query Port		*/	$bot['query_port'] = "10011";
-/*	Query Kullanıcı		*/	$bot['query_login'] = "serveradmin";
-/*	Query Şifresi		*/	$bot['query_password'] = "";
-/*	Bot İsmi	*/	$bot['query_nickname'] = "DDosGuard";
+/*	Server IP		*/	$tsayar['server_ip'] = "127.0.0.1";
+/*	UDP Port		*/	$tsayar['server_port'] = "9987";
+/*	Query Port		*/	$tsayar['query_port'] = "10011";
+/*	Query Kullanıcı		*/	$tsayar['query_login'] = "serveradmin";
+/*	Query Şifresi		*/	$tsayar['query_password'] = "";
+/*	Bot İsmi	*/	$tsayar['query_nickname'] = "DDoSKoruma";
 
 ?>
