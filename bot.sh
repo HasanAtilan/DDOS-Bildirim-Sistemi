26bf5d6fbebb9d23f0031ba7b5c94017c233c313
HasanAtilan <zb-sunucum@hotmail.com>
2018-09-22 17:25:24 +0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # Renkler
 ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-COL_GREEN=$ESC_SEQ"32;01m"
-COL_GOLD=$ESC_SEQ"30;33m"
-COL_RED=$ESC_SEQ"31;02m"
+kirmizi=$ESC_SEQ"31;02m"
 
-echo -e "$COL_RED
+echo -e "$kirmizi
                   
-                                             $COL_GOLD  
+                                             $kirmizi  
                  ╔/════════════════╔๑ஜ۩۞۩ஜ๑╗════════════════\╗
                  ║                                          ║
                  ║        ♦ Script created by hasanatilan ♦        ║
@@ -16,17 +13,17 @@ echo -e "$COL_RED
                  ║           ♦ Hasan Atilan ♦            ║
                  ║                                          ║
                  ╚\════════════════╚๑ஜ۩۞۩ஜ๑╝════════════════/╝
-				 $COL_RESET
+				 $kirmizi
 				 "
  
-if [ $1 = 'stop' ] 
+if [ $1 = 'durdur' ] 
     then 
-        pkill -f DdosGuard_Bot
-		echo -e "DdosGuard_Bot: $COL_GREEN Bot Durduruldu! $COL_RESET"
+        pkill -f DDOSBildirim
+		echo -e "DDOSBildirim: $COL_GREEN Bot Durduruldu! $COL_RESET"
     fi
 
-if [ $1 = 'start' ] 
+if [ $1 = 'baslat' ] 
     then 
-        screen -A -m -d -S DdosGuard_Bot php ddosguard.php
-		echo -e "DdosGuard_Bot: $COL_GREEN Bot Başlatıldı! $COL_RESET"
+        screen -A -m -d -S DDOSBildirim php ddosguard.php
+		echo -e "DDOSBildirim: $COL_GREEN Bot Başlatıldı! $COL_RESET"
     fi
